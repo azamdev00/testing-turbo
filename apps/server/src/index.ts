9b8319c378d299ee19b3cfc9c0283258e0f25836
Azam <azamtanoli00@gmail.com>
2023-03-01 21:14:12 +0500

    first commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.json({
+    status: "success",
+    message: "Turborepo version 1.0.0....",
+  });
+});
+
+app.listen(8000, () => {
+  console.log("Server is listening on port 8000");
+});
